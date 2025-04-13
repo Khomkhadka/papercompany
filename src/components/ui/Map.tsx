fccf98e665b9b18291a016a122abbc600d750048
Khomkhadka <khomkhadka67@gmail.com>
2025-04-13 17:50:29 +0545

    something

---
+++ src/components/ui/Map.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const center: [number, number] = [27.854721257860916, 82.67264918650716];
+
+const App = () => {
+  return (
+    <div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d998.7168376403724!2d82.67223872168367!3d27.85450915100601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3997a5133dc4307b%3A0x86332014cb2840fb!2sRAPTI%20PAPER%20%26%20HAND%20CRAFT!5e1!3m2!1sen!2snp!4v1744364009007!5m2!1sen!2snp" width="100%" height="450"  loading="lazy" ></iframe>
+   </div>
+  );
+};
+
+export default App;
